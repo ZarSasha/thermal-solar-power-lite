eae48321ca643ef45b887c91086492c94cdb272f
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2025-12-12 20:22:59 +0100

    small fixes

--- migrations/2.2.0.lua
+++ migrations/2.2.0.lua
@@ -2,14 +2,11 @@
 -- MIGRATIONS FOR V2.2.0
 ---------------------------------------------------------------------------------------------------
 
--- Change to name of storage table key, that contain thermal solar panel IDs:
+-- Name change for storage table key:
 if storage.tspl_thermal_panel_table ~= nil then
-    -- Transfers data from old to new key:
     storage.thermal_panels = storage.tspl_thermal_panel_table
-    -- Cleans up the old key:
     storage.tspl_thermal_panel_table = nil
-    -- Logs a message to the console for debugging/confirmation:
-    log("Migrated data from 'tspl_thermal_panel_table' to 'panel_IDs'.")
+    log("Migrated data from 'storage.tspl_thermal_panel_table' to 'storage.thermal_panels'.")
 end
 
 ---------------------------------------------------------------------------------------------------

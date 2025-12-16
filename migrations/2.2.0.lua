bd4119d9109895db05c59e8d0077959e85ba1c25
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2025-12-16 18:34:55 +0100

    Update 2.2.0.lua

--- migrations/2.2.0.lua
+++ migrations/2.2.0.lua
@@ -1,22 +1,29 @@
 ---------------------------------------------------------------------------------------------------
 -- MIGRATIONS FOR V2.2.0
 ---------------------------------------------------------------------------------------------------
+-- The new time slicing feature needs several variables to be stored within the storage table. The
+-- main table that holds the string identifiers also needs to replaced with a simple array
+-- (contiguous indexed table).
 
-if storage.panels               == nil then storage.panels               =    {} end
-if storage.panels.main          == nil then storage.panels.main          =    {} end
-if storage.panels.to_be_added   == nil then storage.panels.to_be_added   =    {} end
-if storage.panels.to_be_removed == nil then storage.panels.to_be_removed =    {} end
-if storage.panels.batch_size    == nil then storage.panels.batch_size    =    10 end -- small, for testing
-if storage.panels.progress      == nil then storage.panels.progress      =     1 end
-if storage.panels.complete      == nil then storage.panels.complete      = false end
+-- Adds new table to storage.
+storage.panels = {
+    main          = {},
+    to_be_added   = {},
+    to_be_removed = {},
+    batch_size    = 10,
+    progress      =  1,
+    complete      = false
+}
 
 -- Transfer of data from storage table to new variable:
 if storage.tspl_thermal_panel_table ~= nil then
-    -- Extracts values (ignoring keys) and copies them into new array:
+    -- Extracts values (ignoring keys) from table and copies them into new array in storage:
     for _, v in pairs(storage.tspl_thermal_panel_table) do
         table.insert(storage.panels.main, v)
     end
+    -- Deletes old table in storage:
     table_clear(storage.tspl_thermal_panel_table)
+    storage.tspl_thermal_panel_table = nil
     log(
         "Migrated data from table: 'storage.tspl_thermal_panel_table' "
       .."to new array: 'storage.panels.main'."

fcef87d6e836e6afde6658cf22f6ffafb6b65d94
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2025-12-16 17:49:34 +0100

    Update 2.2.0.lua

--- migrations/2.2.0.lua
+++ migrations/2.2.0.lua
@@ -2,13 +2,16 @@
 -- MIGRATIONS FOR V2.2.0
 ---------------------------------------------------------------------------------------------------
 
+if storage.panels               == nil then storage.panels               =    {} end
+if storage.panels.main          == nil then storage.panels.main          =    {} end
+
 -- Transfer of data from storage table to new variable:
 if storage.tspl_thermal_panel_table ~= nil then
     -- Extracts values (ignoring keys) and copies them into new array:
     for _, v in pairs(storage.tspl_thermal_panel_table) do
         table.insert(storage.panels.main, v)
     end
-    storage.tspl_thermal_panel_table = nil
+    table_clear(storage.tspl_thermal_panel_table)
     log(
         "Migrated data from table: 'storage.tspl_thermal_panel_table' "
       .."to new array: 'storage.panels.main'."

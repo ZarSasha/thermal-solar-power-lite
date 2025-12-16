6fb818fecfeef6bb071c74868c981886d62f5d2f
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2025-12-16 18:00:36 +0100

    Update 2.2.0.lua

--- migrations/2.2.0.lua
+++ migrations/2.2.0.lua
@@ -4,6 +4,11 @@
 
 if storage.panels               == nil then storage.panels               =    {} end
 if storage.panels.main          == nil then storage.panels.main          =    {} end
+if storage.panels.to_be_added   == nil then storage.panels.to_be_added   =    {} end
+if storage.panels.to_be_removed == nil then storage.panels.to_be_removed =    {} end
+if storage.panels.batch_size    == nil then storage.panels.batch_size    =    10 end -- small, for testing
+if storage.panels.progress      == nil then storage.panels.progress      =     1 end
+if storage.panels.complete      == nil then storage.panels.complete      = false end
 
 -- Transfer of data from storage table to new variable:
 if storage.tspl_thermal_panel_table ~= nil then

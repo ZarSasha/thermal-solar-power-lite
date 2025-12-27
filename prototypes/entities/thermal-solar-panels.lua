02da9b1ba1eb3ee92d3ca87025ca4001bd85cf69
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2025-12-27 15:52:25 +0100

    fix

--- prototypes/entities/thermal-solar-panels.lua
+++ prototypes/entities/thermal-solar-panels.lua
@@ -42,7 +42,7 @@ local hr_panel_disconnection_sprites = {
 -- COMPATIBILITY for Pyanodon Coal Processing --
 local heat_capacity_kJ = 50
 if MOD.PY_COAL_PROCESSING then
-    -- Compensates for halved steam conversion efficiency and some complex heat loss:
+    -- Compensates for halved steam conversion efficiency:
     heat_capacity_kJ = 100
     -- Note: Runtime script also adapted for 250°C steam, if option is selected.
 end

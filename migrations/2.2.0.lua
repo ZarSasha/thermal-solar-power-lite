dd18fc5d6b54042f1fb771182d0104169e3d8108
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-10 18:59:26 +0200

    fixes

--- migrations/2.2.0.lua
+++ migrations/2.2.0.lua
@@ -27,9 +27,11 @@ if storage.tspl_thermal_panel_table ~= nil then
 end
 
 -- Message to console:
-game.print("[color=acid]Thermal Solar Power (Lite):[/color]")
-game.print("  Regarding update to v2.2.0: It is recommended to read the changelog!")
-game.print("  If panels don't work, please report the issue on the Mod Portal or on GitHub.")
-game.print("  However, writing '/tspl reset' in the console should quickly resolve any issues.")
+if script.active_mods["thermal-solar-power-lite"].version == "2.2.0" then
+    game.print("[color=acid]Thermal Solar Power (Lite):[/color]")
+    game.print("  Regarding update to v2.2.0: It is recommended to read the changelog!")
+    game.print("  If panels don't work, please report the issue on the Mod Portal or on GitHub.")
+    game.print("  However, writing '/tspl reset' in the console should quickly resolve any issues.")
+end
 
 ---------------------------------------------------------------------------------------------------

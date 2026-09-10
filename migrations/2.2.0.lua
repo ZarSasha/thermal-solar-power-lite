df95ddd8af817ddd09dea6172a986cf4beb50ba1
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-10 19:04:01 +0200

    Update 2.2.0.lua

--- migrations/2.2.0.lua
+++ migrations/2.2.0.lua
@@ -1,6 +1,9 @@
 ---------------------------------------------------------------------------------------------------
 -- MIGRATIONS FOR V2.2.0
 ---------------------------------------------------------------------------------------------------
+
+require "shared.all-stages"
+
 -- The new time slicing feature needs several variables to be stored within the storage table. The
 -- main table that holds the string identifiers also needs to be replaced with a simple array.
 
@@ -27,7 +30,7 @@ if storage.tspl_thermal_panel_table ~= nil then
 end
 
 -- Message to console:
-if script.active_mods["thermal-solar-power-lite"].version == "2.2.0" then
+if script.active_mods[MOD_NAME].version == "2.2.0" then
     game.print("[color=acid]Thermal Solar Power (Lite):[/color]")
     game.print("  Regarding update to v2.2.0: It is recommended to read the changelog!")
     game.print("  If panels don't work, please report the issue on the Mod Portal or on GitHub.")

07f1a87b04c18a8da72a32948afdfb4ad4d21bb5
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-14 00:13:09 +0200

    Update 2.4.2.lua

--- migrations/2.4.2.lua
+++ migrations/2.4.2.lua
@@ -6,8 +6,8 @@ require "shared.all-stages"
 
 if script.active_mods[MOD_NAME].version == "2.4.2" then
     game.print("[color=acid]Thermal Solar Power (Lite):[/color]")
-    game.print("  v2.4.2: The default values of the settings have been changed! Note that")
-    game.print("  the current values haven't been changed, since they are player-owned.")
+    game.print("  v2.4.2: The default values of the settings have been changed! The currently")
+    game.print("  selected values should not have been changed, since they are player-owned.")
 end
 
 ---------------------------------------------------------------------------------------------------

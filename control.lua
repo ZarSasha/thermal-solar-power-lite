727d689b24b41451acebfb0deee61fe6d0a30cc6
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-13 17:07:48 +0200

    Update control.lua

--- control.lua
+++ control.lua
@@ -49,8 +49,8 @@ local base_temp_loss   = temp_loss_factor * tick_frequency
 -- Pyanodon Coal Processing:
 if script.active_mods["pycoalprocessing"] and SETTING.select_mod == "Pyanodon" then
     -- Decreases heat loss rate to allow similar efficiency at 250°C (compared to 165°C).
-    --temp_loss_factor = 0.00314 -- "correct" value: 0.0031915
-    base_temp_gain = ((SETTING.panel_output_kW * tick_frequency) / heat_cap_kJ) * 1.32
+    temp_loss_factor = 0.00314 -- "correct" value: 0.0031915
+    base_temp_gain = ((SETTING.panel_output_kW * tick_frequency) / heat_cap_kJ) * 1.252
 end
 
 -- More Quality Scaling:

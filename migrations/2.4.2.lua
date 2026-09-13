3d094fa2649bb733939458be90c84dc6768b974c
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-14 01:02:53 +0200

    fixes

--- migrations/2.4.2.lua
+++ migrations/2.4.2.lua
@@ -2,10 +2,8 @@
 -- MIGRATIONS FOR V2.4.2
 ---------------------------------------------------------------------------------------------------
 
-if script.active_mods["thermal-solar-power-lite"].version == "2.4.2" then
-    game.print("[color=acid]Thermal Solar Power (Lite):[/color]")
-    game.print("  v2.4.2: The default values of the settings have been changed! The currently")
-    game.print("  selected values should not have been changed, since they are player-owned.")
-end
+game.print("[color=acid]Thermal Solar Power (Lite):[/color]")
+game.print("  v2.4.2: The default values of the settings have been changed! The currently")
+game.print("  selected values should not have been changed, since they are player-owned.")
 
 ---------------------------------------------------------------------------------------------------

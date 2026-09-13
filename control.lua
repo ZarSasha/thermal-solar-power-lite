080ffc716fcc6cf71c26e6a275647028b2687485
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-13 14:46:27 +0200

    Update control.lua

--- control.lua
+++ control.lua
@@ -57,7 +57,7 @@ if ACTIVE_MODS.PY_COAL_PROCESSING and SETTING.select_mod == "Pyanodon" then
     -- Decreases heat loss rate to allow similar efficiency at 250°C (compared to 165°C).
     -- Also accounts for doubled heat capacity of panels, which keeps temperatures higher
     -- during night and thus slightly increases heat energy loss.
-    temp_loss_factor = 0.00314 -- "correct" value: 0.0031915
+    temp_loss_factor = 0.00427 --
 end
 
 -- More Quality Scaling:

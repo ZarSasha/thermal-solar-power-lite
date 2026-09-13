87781a053384f62f646d4d99053636ad4df4dfa0
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-13 20:57:24 +0200

    Update control.lua

--- control.lua
+++ control.lua
@@ -37,7 +37,7 @@ local min_batch_size = 3  -- 3 * 58 = 174 panels before batch size must increase
 
 -- Parameters pertaining to the thermal solar panels:
 local heat_cap_kJ      = 50    -- default value, will not be changed
-local temp_loss_factor = 0.005 -- updated during startup
+local temp_loss_factor = 0.005 -- updated during startup,
 local quality_scaling  = 0.15  -- updated during startup
 local base_temp_gain   = (SETTING.panel_output_kW * tick_frequency) / heat_cap_kJ
 local base_temp_loss   = temp_loss_factor * tick_frequency
@@ -48,8 +48,7 @@ local base_temp_loss   = temp_loss_factor * tick_frequency
 
 -- Pyanodon Coal Processing:
 if script.active_mods["pycoalprocessing"] and SETTING.select_mod == "Pyanodon" then
-    -- Multiplies temp increase by the square root of (250-15)/(165-15) = 1.566.
-    base_temp_gain = ((SETTING.panel_output_kW * tick_frequency) / heat_cap_kJ) * 1.252
+    temp_loss_factor = 0.00314
 end
 
 -- More Quality Scaling:
@@ -57,8 +56,6 @@ if script.active_mods["more-quality-scaling"] and table_contains_value(
     {"capacity", "both"}, settings.startup["mqs-heat-changes"].value) then
     -- Nullifies quality scaling factor, since heat capacity scales instead (30% pr. level):
     quality_scaling = 0
-else
-    quality_scaling = 0.15
 end
 
 ---------------------------------------------------------------------------------------------------

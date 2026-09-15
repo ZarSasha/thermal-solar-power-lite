6e041776e41a7c27d9d4e1088a82b70fa332eee3
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-09-16 00:42:24 +0200

    additions
    updated changelog, added migration script.

---
+++ migrations/2.6.0.lua
@@ -0,0 +1,10 @@
+---------------------------------------------------------------------------------------------------
+-- MIGRATIONS FOR V2.6.0
+---------------------------------------------------------------------------------------------------
+
+game.print("[color=acid]Thermal Solar Power (Lite):[/color]")
+game.print("  v2.6.0: NEW FEATURE: The basic heat exchanger can now be configured to output")
+game.print("  hot water instead of steam! Read changelog or mod page for more details.")
+
+
+---------------------------------------------------------------------------------------------------
